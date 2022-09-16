61a7dfb64e6f81c0e9116b86db06e8eaef96b395
Виктор <viktor@MacBook-Air-Viktor.local>
2022-09-16 10:04:54 +0300

    change-2 375px

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,18 +1,11 @@
 import bill from "./help.png";
-import binance from "./binance.png";
-import card from "./card.png";
-import coinbase from "./coinbase.png";
-import dropbox from "./dropbox.png";
 import logo from "./logo.png";
 import quotes from "./quotes.svg";
-import robot from "./robot.png";
 import send from "./Send.svg";
 import shield from "./Shield.svg";
 import star from "./Star.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import google from "./google.svg";
-import apple from "./apple.svg";
 import arrowUp from "./arrow-up.svg";
 import discount from "./Discount.svg";
 import tiktok from "./tiktok.svg";
@@ -23,27 +16,20 @@ import viber from "./viber.svg";
 import people01 from "./people01.jpg";
 import people02 from "./people02.jpg";
 import people03 from "./people03.jpg";
-import plumber from "./plumber2.png";
+import plumber from "./plumber2-min.png";
 import mask from "./mask.png";
 import phone from "./phone.png";
 
 export {
   bill,
-  binance,
-  card,
-  coinbase,
-  dropbox,
   logo,
   quotes,
-  robot,
   clock,
   send,
   shield,
   star,
   menu,
   close,
-  google,
-  apple,
   arrowUp,
   discount,
   tiktok,
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
       {isModal &&
         <Modal title={"Вызвать мастера"} setIsModal={setIsModal} />
       }
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 py-24`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 py-24 xy:pb-3`}>
         <div className="flex flex-row items-center py-[6px] px-4 xy:px-2 bg-discount-gradient rounded-[10px] mb-2">
           <p className={`${styles.paragraph} xy:text-[15px] xs:text-[18px] ml-2 xy:ml-0`}>
             <span className="text-white">Выезд</span> при выполнении {" "}
